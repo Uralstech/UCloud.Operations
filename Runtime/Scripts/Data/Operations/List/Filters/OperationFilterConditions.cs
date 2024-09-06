b31306bdced7a679d542a5a2a701998212c3db7e
Udayshankar Ravikumar <uralstech@gmail.com>
2024-09-07 00:08:53 +0530

    Added filters for list requests.

---
+++ Runtime/Scripts/Data/Operations/List/Filters/OperationFilterConditions.cs
@@ -0,0 +1,31 @@
+﻿namespace Uralstech.UCloud.Operations
+{
+    /// <summary>
+    /// Conditions to filter operations by.
+    /// </summary>
+    public class OperationFilterConditions
+    {
+        /// <summary>
+        /// The Left-Hand-Side operand.
+        /// </summary>
+        public OperationFilterConditionOperand OperandA;
+
+        /// <summary>
+        /// The operator.
+        /// </summary>
+        public OperationFilterOperator Operator;
+
+        /// <summary>
+        /// The Right-Hand-Side operand.
+        /// </summary>
+        public OperationFilterConditions OperandB;
+
+        /// <inheritdoc/>
+        public override string ToString()
+        {
+            return Operator == OperationFilterOperator.None
+                ? OperandA.ToString()
+                : $"{OperandA}{Operator}{OperandB}";
+        }
+    }
+}

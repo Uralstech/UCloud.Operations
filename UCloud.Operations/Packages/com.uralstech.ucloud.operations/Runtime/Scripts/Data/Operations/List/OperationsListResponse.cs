60526b17291bfbf191463d872ab9ea8cc14914f5
Udayshankar Ravikumar <uralstech@gmail.com>
2024-09-07 16:08:24 +0530

    Made list request simpler.

--- UCloud.Operations/Packages/com.uralstech.ucloud.operations/Runtime/Scripts/Data/Operations/List/OperationsListResponse.cs
+++ UCloud.Operations/Packages/com.uralstech.ucloud.operations/Runtime/Scripts/Data/Operations/List/OperationsListResponse.cs
@@ -7,16 +7,5 @@ namespace Uralstech.UCloud.Operations
     /// The response for an <see cref="OperationsListRequest"/> call.
     /// </summary>
     [JsonObject(NamingStrategyType = typeof(CamelCaseNamingStrategy))]
-    public class OperationsListResponse
-    {
-        /// <summary>
-        /// A list of operations that matches the specified filter in the request.
-        /// </summary>
-        public Operation[] Operations;
-
-        /// <summary>
-        /// A token that can be sent as a <see cref="OperationsListRequest.PageToken"/> into a subsequent <see cref="OperationsListRequest"/> call.
-        /// </summary>
-        public string NextPageToken;
-    }
+    public class OperationsListResponse : Generic.OperationsListResponse<Operation> {}
 }

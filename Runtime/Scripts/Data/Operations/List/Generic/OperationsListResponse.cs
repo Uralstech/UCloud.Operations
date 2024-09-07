528931f5090a7af07a7db85b86c5c75030b79f26
Udayshankar Ravikumar <uralstech@gmail.com>
2024-09-07 16:08:24 +0530

    Made list request simpler.

--- Runtime/Scripts/Data/Operations/List/Generic/OperationsListResponse.cs
+++ Runtime/Scripts/Data/Operations/List/Generic/OperationsListResponse.cs
@@ -6,15 +6,14 @@ namespace Uralstech.UCloud.Operations.Generic
     /// <summary>
     /// The generic response for an <see cref="OperationsListRequest"/> call.
     /// </summary>
-    /// <typeparam name="TMetadata">The type of the operation's metadata.</typeparam>
-    /// <typeparam name="TResponse">The type of the operation's response.</typeparam>
+    /// <typeparam name="TOperation">The type of the operation object.</typeparam>
     [JsonObject(NamingStrategyType = typeof(CamelCaseNamingStrategy))]
-    public class OperationsListResponse<TMetadata, TResponse>
+    public class OperationsListResponse<TOperation>
     {
         /// <summary>
         /// A list of operations that matches the specified filter in the request.
         /// </summary>
-        public Operation<TMetadata, TResponse>[] Operations;
+        public TOperation[] Operations;
 
         /// <summary>
         /// A token that can be sent as a <see cref="OperationsListRequest.PageToken"/> into a subsequent <see cref="OperationsListRequest"/> call.
